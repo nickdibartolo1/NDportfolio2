54dad96c6367020348b8278dd710f427f6fd7e53
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-04-09 23:15:24 -0400

    added modal and styles to about me, created files for future footer

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,20 +1,46 @@
-import img from '../../assets/headshot.jpg'
-import './about.css'
+import img from '../../assets/headshot.jpg';
+import './about.css';
+import { useDisclosure } from '@mantine/hooks';
+import { Modal } from '@mantine/core';
 
 const About = () => {
+    const [opened, { open, close }] = useDisclosure(false);
+
     return (
         <>
-            <h1 className="header">About Me</h1>
-            <section className='headshot_container'>
-                <div className='headshot_frame'>
-                    <div>
-                        <div>
-                            <img className="headshot" src={img} alt="Image of Nick DiBartolo" />
-                        </div>
-                        <p></p>
+            <div className="about_text_alignment">
+                <h1 className="about_text">About Me</h1>
+            </div>
+            <div className='headshot_container'>
+                <div onClick={open} className='headshot_frame'>
+                    <img className="headshot" src={img} alt="Image of Nick DiBartolo" />
+                    <div className="headshot_text--margin">
+                        <span className="headshot_text">Click Me!</span>
                     </div>
+                </div>
+            </div>
+            <Modal size="55%" opened={opened} onClose={close} centered>
+                <section className='modal_container'>
+                    <img className="modal_headshot" src={img} alt="Image of Nick DiBartolo"></img>
+                    <p className="text">I am a Junior Developer named Nick DiBartolo and I was born and raised in Northern New Jersey.
+                        My tech journey began when I transitioned into coding and
+                        web development in the later time I spent in college.
+                        I was originally an Education major at Montclair State University, but decided to switch as I craved something more challenging.
+                        I enrolled in a Full-Stack Web Development Bootcamp at Rutgers University
+                        where I received a certification in Full-Stack Web Development.
+                        I am currently working as a Junior Developer at Strategic Micro Systems,
+                        an IT company in Parsippany, New Jersey. In my free time, I love to workout, play guitar, and read about history.
+                    </p>
+                </section>
+            </Modal>
+        </>
+    )
+}
 
-                    {/* <div>
+export default About
+
+
+{/* <div>
                 <p className="text">I was born and raised in Northern New Jersey and transitioned into coding,
                     web development, and the tech field later in college.
                     I was originally an Education major at Montclair State University, but decided to switch as I craved something more challenging.
@@ -23,10 +49,3 @@ const About = () => {
                     an IT company in Parsippany, New Jersey. In my free time, I love to workout, play guitar, and read about history.
                 </p>
             </div> */}
-                </div>
-            </section>
-        </>
-    )
-}
-
-export default About
