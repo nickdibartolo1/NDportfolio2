68e87dbf0242dadbe994b5e80cb586dc702c50f7
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2024-04-16 16:48:14 -0400

    added pictures to app, working with mapping them

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,40 +1,48 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import ndportfolio1 from "../img/portfolio1.png"
+import nflfanbase from '../img/nflfanbase.png'
+import randomQ from "../img/randomQuote.png"
+import img from "../../assets/img/headshot.jpg";
+
 const data = [
     {
-        image:
-            '',
-        title: 'Best forests to visit in North America',
+        id: uuidv4(),
+        image: img,
+        title: 'DASH App',
         category: 'nature',
     },
     {
-        image:
-            '',
-        title: 'Hawaii beaches review: better than you think',
-        category: 'beach',
-    },
-    {
-        image:
-            '',
-        title: 'Mountains at night: 12 best locations to enjoy the view',
+        id: uuidv4(),
+        image: img,
+        title: 'Word Match Game',
         category: 'nature',
     },
     {
-        image:
-            '',
-        title: 'Aurora in Norway: when to visit for best experience',
+        id: uuidv4(),
+        image: nflfanbase,
+        title: 'NFL Fanbase',
         category: 'nature',
     },
     {
-        image:
-            '',
-        title: 'Best places to visit this winter',
-        category: 'tourism',
-    },
-    {
-        image:
-            '',
-        title: 'Active volcanos reviews: travel at your own risk',
+        id: uuidv4(),
+        image: img,
+        title: "Nick's Current Portfolio",
         category: 'nature',
     },
+    {
+        id: uuidv4(),
+        image: ndportfolio1,
+        title: "Nick's Old Portfolio",
+        category: 'nature',
+    },
+    {
+        id: uuidv4(),
+        image: randomQ,
+        title: "Random Quote Generator",
+        category: 'nature',
+    },
+
 ];
 
 export default data
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,9 +1,108 @@
+// import { useEffect } from 'react';
+// import { Carousel } from '@mantine/carousel';
+// import './projects.css'
+// import img from "../../assets/img/headshot.jpg";
+// import project_data from "../../assets/data/data.js"
+
+
+// function Projects() {
+
+//     useEffect(() => {
+//         const info = project_data.map((data, index) => {
+//             return (
+//                 <Carousel.Slide key={index}>
+//                     <div className=''>
+//                         <img className='img' src={data.img} />
+//                         <div>sgsdga</div>
+//                     </div>
+//                 </Carousel.Slide>
+//             )
+//         })
+
+//     }, [])
+
+
+//     return (
+//         <>
+//             <div className="header__alignment">
+//                 <h1 className="header__text">Projects</h1>
+//             </div>
+
+//             <section className='carousel__margin'>
+//                 <Carousel height={800}
+//                     slideSize="33.333333%"
+//                     slideGap="xl"
+//                     align="start"
+//                     controlSize={60}
+//                     className='carousel'>
+
+//                         {info}
+
+//                     {/* <Carousel.Slide>
+//                         <div className=''>
+//                             <img className='img' src={img} />
+//                             <div>sgsdga</div>
+//                         </div>
+//                     </Carousel.Slide>
+
+//                     <Carousel.Slide>
+//                         <div className=''>
+//                             <img className='img' src={img} />
+//                         </div>
+//                     </Carousel.Slide>
+
+//                     <Carousel.Slide>
+//                         <div className=''>
+//                             <img className='img' src={img} />
+//                         </div>
+//                     </Carousel.Slide>
+
+//                     <Carousel.Slide>
+//                         <div className=''>
+//                             <img className='img' src={img} />
+//                         </div>
+//                     </Carousel.Slide>
+
+//                     <Carousel.Slide>
+//                         <div className=''>
+//                             <img className='img' src={img} />
+//                         </div>
+//                     </Carousel.Slide> */}
+
+//                     {/* ...other slides */}
+//                 </Carousel>
+//             </section>
+//         </>
+
+
+//     );
+// }
+
+// export default Projects
+
+import { useEffect, useState } from 'react';
 import { Carousel } from '@mantine/carousel';
 import './projects.css'
-import img from "../../assets/img/headshot.jpg";
-import data from "../../assets/data/data.js"
+// import img from "../../assets/img/headshot.jpg";
+import project_data from "../../assets/data/data.js"
 
 function Projects() {
+    const [info, setInfo] = useState([]);
+
+
+    useEffect(() => {
+        const infoData = project_data.map((data, index) => (
+            <Carousel.Slide key={index}>
+                <div className=''>
+                    <img className='img' src={data.img} alt={`Project ${index}`} />
+                    {/* <div>{data.description}</div> */}
+                </div>
+            </Carousel.Slide>
+        ));
+        setInfo(infoData);
+    }, []);
+
+
     return (
         <>
             <div className="header__alignment">
@@ -11,51 +110,18 @@ function Projects() {
             </div>
 
             <section className='carousel__margin'>
-                <Carousel height={800}
+                <Carousel
+                    height={800}
                     slideSize="33.333333%"
                     slideGap="xl"
                     align="start"
                     controlSize={60}
                     className='carousel'>
-
-                    <Carousel.Slide>
-                        <div className=''>
-                            <img className='img' src={img} />
-                            <div>sgsdga</div>
-                        </div>
-                    </Carousel.Slide>
-
-                    <Carousel.Slide>
-                        <div className=''>
-                            <img className='img' src={img} />
-                        </div>
-                    </Carousel.Slide>
-
-                    <Carousel.Slide>
-                        <div className=''>
-                            <img className='img' src={img} />
-                        </div>
-                    </Carousel.Slide>
-
-                    <Carousel.Slide>
-                        <div className=''>
-                            <img className='img' src={img} />
-                        </div>
-                    </Carousel.Slide>
-
-                    <Carousel.Slide>
-                        <div className=''>
-                            <img className='img' src={img} />
-                        </div>
-                    </Carousel.Slide>
-
-                    {/* ...other slides */}
+                    {info}
                 </Carousel>
             </section>
         </>
-
-
     );
 }
 
-export default Projects
+export default Projects;
